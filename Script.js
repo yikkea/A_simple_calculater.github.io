11ce7fb0daa673c577df3f4de1796e2094f2fc7a
yikkea <farhasifkhan25@gmail.com>
2026-06-05 21:56:25 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,28 @@
+// publish page please 
+function plus() {
+ let n1 = document.getElementById("n1").value;
+ let n2 = document.getElementById("n2").value;
+ let p = document.getElementById("result")
+ p.innerHTML = n1 + n2 ;
+}
+
+function minus() {
+ let n1 = document.getElementById("n1").value;
+ let n2 = document.getElementById("n2").value;
+ let p = document.getElementById("result")
+ p.innerHTML = n1 - n2 ;
+}
+
+function divide() {
+ let n1 = document.getElementById("n1").value;
+ let n2 = document.getElementById("n2").value;
+ let p = document.getElementById("result")
+ p.innerHTML = n1 / n2 ;
+}
+
+function multiply() {
+ let n1 = document.getElementById("n1").value;
+ let n2 = document.getElementById("n2").value;
+ let p = document.getElementById("result")
+ p.innerHTML = n1 * n2 ;
+}
