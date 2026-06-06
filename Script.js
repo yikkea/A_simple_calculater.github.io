9dc96dff734612472bf5e23c7cb9827f45bbf6d8
yikkea <farhasifkhan25@gmail.com>
2026-06-07 00:01:23 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,34 @@
+// publish page please 
+function plus() {
+ let n1 = document.getElementById("n1").value;
+ let n2 = document.getElementById("n2").value;
+ let p = document.getElementById("result")
+ p.innerHTML = Number(n1) + Number(n2) ;
+}
+
+function minus() {
+ let n1 = document.getElementById("n1").value;
+ let n2 = document.getElementById("n2").value;
+ let p = document.getElementById("result")
+ p.innerHTML = n1 - n2 ;
+}
+
+function divide() {
+ let n1 = document.getElementById("n1").value;
+ let n2 = document.getElementById("n2").value;
+ let p = document.getElementById("result")
+ p.innerHTML = n1 / n2 ;
+}
+
+function multiply() {
+ let n1 = document.getElementById("n1").value;
+ let n2 = document.getElementById("n2").value;
+ let p = document.getElementById("result")
+ p.innerHTML = n1 * n2 ;
+}
+
+function clearall() {
+  document.getElementById("n1").value = "";
+  document.getElementById("n2").value = "";
+  document.getElementById("result").innerHTML = "";
+}
